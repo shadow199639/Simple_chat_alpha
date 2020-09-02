c3348144cb01a67567fdc277407339a3eda63fa7
Bobur <58975973+shadow199639@users.noreply.github.com>
2020-09-02 12:23:34 +0500

    Add files via upload

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -0,0 +1,26 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+
+class Profile (
+    val firstName: String,
+    val lastName: String,
+    val about: String,
+    val repository: String,
+    val rating: Int = 0,
+    val respect: Int = 0
+){
+    val rank: String = "Junior Android Developer"
+    val nickname: String = Utils.transliteration("$firstName $lastName", "_")
+
+    fun toMap(): Map<String, Any> = mapOf(
+        "nickname" to nickname,
+        "rank" to rank,
+        "firstName" to firstName,
+        "lastName" to lastName,
+        "about" to about,
+        "repository" to repository,
+        "rating" to rating,
+        "respect" to respect
+    )
+}
